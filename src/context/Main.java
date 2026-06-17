9ab43cc06fdee26fd50119ee3889b1b43ebf7dd4
Juan Manuel Eraso <juaneraso2003@hotmail.com>
2026-06-17 10:34:50 -0500

    Vigesimo Septimo ejercicio APOII reorganizado

---
+++ src/context/Main.java
@@ -0,0 +1,14 @@
+package context;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Mensaje
+        JOptionPane.showMessageDialog(
+                null,
+                "Hola Mundo"
+        );
+    }
+}
